a1c5be0d3f5ff4d4351e1290aa3506046780fe10
kayode alegbemi <kayode.alegbemi@gmail.com>
2024-06-21 14:38:11 -0700

    Loop to language

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const wordArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+const arrLength = wordArray.length;
+
+while (i < arrLength) {
+  console.log(wordArray[i]);
+  i++;
+}
